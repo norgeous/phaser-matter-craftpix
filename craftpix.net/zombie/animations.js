e48c015527103e13f15cec38660683066fcb0c9f
norgeous <daniel@harte.me>
2022-12-13 21:16:28 +0000

    file

---
+++ craftpix.net/zombie/animations.js
@@ -0,0 +1,7 @@
+export default {
+  zombie_attack: { end: 5, frameRate: 10, repeat: -1 },
+  zombie_death:  { end: 5, frameRate: 10, repeat: -1 },
+  zombie_hurt:   { end: 1, frameRate: 5,  repeat: -1 },
+  zombie_idle:   { end: 3, frameRate: 5,  repeat: -1 },
+  zombie_walk:   { end: 5, frameRate: 10, repeat: -1 },
+};
--- src/phaser/scenes/GameScene.js
+++ src/phaser/scenes/GameScene.js
@@ -1,12 +1,5 @@
 import Phaser from 'phaser';
-
-const animations = {
-  zombie_attack: { end: 5, frameRate: 10, repeat: -1 },
-  zombie_death:  { end: 5, frameRate: 10, repeat: -1 },
-  zombie_hurt:   { end: 1, frameRate: 5,  repeat: -1 },
-  zombie_idle:   { end: 3, frameRate: 5,  repeat: -1 },
-  zombie_walk:   { end: 5, frameRate: 10, repeat: -1 },
-};
+import animations from '../../../craftpix.net/zombie/animations.js';
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -14,7 +7,7 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload () {
-    this.load.atlas('zombie', 'craftpix.net/zombie.png', 'craftpix.net/zombie.json');
+    this.load.atlas('zombie', 'craftpix.net/zombie/spritesheet.png', 'craftpix.net/zombie/atlas.json');
   }
 
   create () {    
@@ -30,7 +23,7 @@ export default class GameScene extends Phaser.Scene {
         repeat,
       });
       
-      this.add.sprite((i*100)+100, 100, 'zombie').play(key);
+      this.add.sprite((i * 100) + 100, 100, 'zombie').play(key);
     });
   }
 
