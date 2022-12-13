c900e4013cf38de90f38745e1afcba494e934b5b
norgeous <daniel@harte.me>
2022-12-13 20:15:09 +0000

    Update GameScene.js

--- src/phaser/scenes/GameScene.js
+++ src/phaser/scenes/GameScene.js
@@ -1,5 +1,12 @@
 import Phaser from 'phaser';
-import EmojiMan from '../objects/EmojiMan';
+
+const animations = {
+  zombie_attack: { end: 5, frameRate: 10, repeat: -1 },
+  zombie_death:  { end: 5, frameRate: 10, repeat: -1 },
+  zombie_hurt:   { end: 1, frameRate: 5,  repeat: -1 },
+  zombie_idle:   { end: 3, frameRate: 5,  repeat: -1 },
+  zombie_walk:   { end: 5, frameRate: 10, repeat: -1 },
+};
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -10,73 +17,43 @@ export default class GameScene extends Phaser.Scene {
     this.load.atlas('zombie', 'craftpix.net/zombie.png', 'craftpix.net/zombie.json');
   }
 
-  create () {
-    this.man1 = new EmojiMan(this, 400, 500, {
-      emojis: {
-        hat: '🎩',
-        head: '😂',
-        body: '👕',
-        hips: '🩳',
-        arm: '💪',
-        hand: '👍',
-        leg: '🦵',
-      },
-    });
-    this.man2 = new EmojiMan(this, 800, 500, {
-      emojis: {
-        hat: '👑',//'🧢',
-        head: '😲',
-        body: '👕',
-        hips: '🩳',
-        arm: '💪',
-        hand: '🖕',
-        leg: '🦵',
-      },
-    });
-    this.man3 = new EmojiMan(this, 1200, 500, {
-      emojis: {
-        hat: '👒',
-        head: '🤖',
-        body: '👚',
-        hips: '🩳',
-        arm: '🦾',
-        hand: '🔪',
-        leg: '🦿',
-      },
-    });
-    
+  create () {    
     this.matter.world.setBounds();
     this.matter.add.mouseSpring();
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    this.anims.create({
-      key: 'zombie_attack',
-      frames: this.anims.generateFrameNames('zombie', { prefix: 'zombie_attack_', end: 5, zeroPad: 4 }),
-      repeat: -1,
-      frameRate: 10,
-    });
-    const zombieAttack = this.add.sprite(100, 100, 'zombie').play('zombie_attack');
+    // this.anims.create({
+    //   key: 'zombie_attack',
+    //   frames: this.anims.generateFrameNames('zombie', { prefix: 'zombie_attack_', end: 5, zeroPad: 4 }),
+    //   repeat: -1,
+    //   frameRate: 10,
+    // });
+    // const zombieAttack = this.add.sprite(100, 100, 'zombie').play('zombie_attack');
 
-    this.anims.create({
-      key: 'zombie_death',
-      frames: this.anims.generateFrameNames('zombie', { prefix: 'zombie_death_', end: 5, zeroPad: 4 }),
-      repeat: -1,
-      frameRate: 10,
-    });
-    const zombieDeath = this.add.sprite(200, 100, 'zombie').play('zombie_death');
+    // this.anims.create({
+    //   key: 'zombie_death',
+    //   frames: this.anims.generateFrameNames('zombie', { prefix: 'zombie_death_', end: 5, zeroPad: 4 }),
+    //   repeat: -1,
+    //   frameRate: 10,
+    // });
+    // const zombieDeath = this.add.sprite(200, 100, 'zombie').play('zombie_death');
 
-    this.anims.create({
-      key: 'zombie_hurt',
-      frames: this.anims.generateFrameNames('zombie', { prefix: 'zombie_hurt_', end: 1, zeroPad: 4 }),
-      repeat: -1,
-      frameRate: 5,
+
+    Object.entries(animations).forEach(([key, { end, frameRate, repeat }], i) => {
+      this.anims.create({
+        key,
+        frames: this.anims.generateFrameNames('zombie', { prefix: `${key}_`, end, zeroPad: 4 }),
+        frameRate,
+        repeat,
+      });
+      
+      this.add.sprite((i*100)+100, 100, 'zombie').play(key);
     });
-    const zombieHurt = this.add.sprite(300, 100, 'zombie').play('zombie_hurt');
   }
 
   update() {
-    this.man1.update();
-    this.man2.update();
-    this.man3.update();
+    // this.man1.update();
+    // this.man2.update();
+    // this.man3.update();
   }
 }
