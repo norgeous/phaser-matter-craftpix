d1bf0ad0e05e630501beb4814a72621a41c1bd38
norgeous <daniel@harte.me>
2022-12-13 17:59:07 +0000

    copy

---
+++ packageConfig.js
@@ -0,0 +1,5 @@
+globalThis.packageConfig = {
+  name: 'phaser-matter-craftpix',
+  version: '0.0.0', // change to invalidate service worker's cache on clientside
+  repository: 'https://github.com/norgeous/phaser-matter-craftpix/',
+};
