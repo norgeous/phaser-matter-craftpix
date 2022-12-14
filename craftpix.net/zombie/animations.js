f79a561dc61434208c19fb232f0699cfc7c3cd42
norgeous <daniel@harte.me>
2022-12-14 18:38:45 +0000

    remove prefix

--- craftpix.net/zombie/animations.js
+++ craftpix.net/zombie/animations.js
@@ -1,7 +1,7 @@
 export default {
-  zombie_attack: { end: 5, frameRate: 10, repeat: -1 },
-  zombie_death:  { end: 5, frameRate: 10, repeat:  0-1 },
-  // zombie_hurt:   { end: 1, frameRate: 5,  repeat: -1 },
-  zombie_idle:   { end: 3, frameRate: 5,  repeat: -1 },
-  zombie_walk:   { end: 5, frameRate: 10, repeat: -1 },
+  attack: { end: 5, frameRate: 10, repeat: -1 },
+  death:  { end: 5, frameRate: 10, repeat:  0-1 },
+  // hurt:   { end: 1, frameRate: 5,  repeat: -1 },
+  idle:   { end: 3, frameRate: 5,  repeat: -1 },
+  walk:   { end: 5, frameRate: 10, repeat: -1 },
 };
