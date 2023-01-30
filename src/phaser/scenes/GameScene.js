c45bd4f6a8b36726db821aa7c779747c5dae283a
Daniel Harte <norgeous@users.noreply.github.com>
2023-01-30 18:32:44 +0000

    load shapes

--- src/phaser/scenes/GameScene.js
+++ src/phaser/scenes/GameScene.js
@@ -13,11 +13,31 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create () {
+    // load map from json
     const map = this.make.tilemap({ key: 'level1' });
     const tileset = map.addTilesetImage('tileset', 'tileset');
     const layer0 = map.createLayer(0, tileset);
-    map.setCollisionByProperty({ collides: true });
-    this.matter.world.convertTilemapLayer(layer0);
+    const ground = map.getObjectLayer('ground');
+    // map.setCollisionByProperty({ collides: true });
+    // this.matter.world.convertTilemapLayer(layer1);
+
+    const { x, y, polygon } = ground.objects[0];
+    const polygonShape = this.add.polygon(0, 0, polygon);
+    const { width, height } = polygonShape;
+    const mgo = this.matter.add.gameObject(polygonShape, {
+      isStatic: true,
+      shape: {
+        type: 'fromVerts',
+        verts: polygon,
+        // flagInternal: true,
+      },
+      position: {
+        x: (width/2) + x,
+        y: (height/2) + y,
+      },
+    });
+    
+    console.log({ x,y, width, height, mgo });
 
     this.matter.world.setBounds();
     this.matter.add.mouseSpring();
