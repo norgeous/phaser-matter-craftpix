351273ad85ba98ce8fe46f7f3f47101a141aacc0
Daniel Harte <norgeous@users.noreply.github.com>
2023-01-30 17:43:10 +0000

    embed tileset data

--- src/phaser/scenes/GameScene.js
+++ src/phaser/scenes/GameScene.js
@@ -8,15 +8,25 @@ export default class GameScene extends Phaser.Scene {
 
   preload () {
     Character.preload(this, 'zombie');
+    this.load.image('tileset', 'original-art/tileset.png');
+    this.load.tilemapTiledJSON('level1', 'original-art/untitled.json');
   }
 
-  create () {    
+  create () {
+    const map = this.make.tilemap({ key: 'level1' });
+    const tileset = map.addTilesetImage('tileset', 'tileset');
+    const layer0 = map.createLayer(0, tileset);
+    map.setCollisionByProperty({ collides: true });
+    this.matter.world.convertTilemapLayer(layer0);
+
     this.matter.world.setBounds();
     this.matter.add.mouseSpring();
     this.cursors = this.input.keyboard.createCursorKeys();
 
     this.test1 = new Character(this, 300,300, { type: 'zombie' });
     // this.test2 = new Character(this, 300,300, { type: 'zombie' });
+
+
   }
 
   update () {
