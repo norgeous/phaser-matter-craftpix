86fd2f17157d2f69fef29ab27e9ac313f3c65363
norgeous <daniel@harte.me>
2023-02-06 18:43:14 +0000

    crow data

--- craftpix.net/crow/index.js
+++ craftpix.net/crow/index.js
@@ -2,19 +2,18 @@ export default {
   animations: {
     idle:   { end: 3, frameRate: 5,  repeat: -1 },
     walk:   { end: 5, frameRate: 10, repeat: -1 },
-    attack: { end: 5, frameRate: 10, repeat: -1 },
-    death:  { end: 5, frameRate: 10, repeat:  0-1 },
+    death:  { end: 3, frameRate: 10, repeat:  0-1 },
     // hurt:   { end: 1, frameRate: 5,  repeat: -1 },
   },
   sprite: {
     offsetX: 2,
-    offsetY: -16.5,
+    offsetY: -10,
   },
   body: {
     shape: {
       type: 'rectangle',
-      width: 20,
-      height: 16,
+      width: 12,
+      height: 12,
     },
     chamfer: { radius: 3 },
   },
