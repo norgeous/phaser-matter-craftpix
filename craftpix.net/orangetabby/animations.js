45730c9a2d3998f178e76ba620d4cdffd5353eaf
norgeous <daniel@harte.me>
2023-02-06 18:10:33 +0000

    remove animations.js

--- craftpix.net/orangetabby/animations.js
+++
@@ -1,7 +0,0 @@
-export default {
-  idle:   { end: 3, frameRate: 5,  repeat: -1 },
-  walk:   { end: 5, frameRate: 10, repeat: -1 },
-  attack: { end: 5, frameRate: 10, repeat: -1 },
-  death:  { end: 5, frameRate: 10, repeat:  0-1 },
-  // hurt:   { end: 1, frameRate: 5,  repeat: -1 },
-};
--- craftpix.net/zombie/index.js
+++ craftpix.net/zombie/index.js
@@ -1,7 +1,11 @@
-import animations from './animations.js';
-
 export default {
-  animations,
+  animations: {
+    idle:   { end: 3, frameRate: 5,  repeat: -1 },
+    walk:   { end: 5, frameRate: 10, repeat: -1 },
+    attack: { end: 5, frameRate: 10, repeat: -1 },
+    death:  { end: 5, frameRate: 10, repeat:  0-1 },
+    // hurt:   { end: 1, frameRate: 5,  repeat: -1 },
+  },
   sprite: {
     offsetX: 10,
     offsetY: -7,
